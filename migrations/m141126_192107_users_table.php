9983d63663ffd867806105c9492c2d6e71096918
dmitriy <nomid21@gmail.com>
2015-01-15 00:13:15 +0200

    user model

---
+++ migrations/m141126_192107_users_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use yii\db\Schema;
+use yii\db\Migration;
+
+class m141126_192107_users_table extends Migration
+{
+    public function up()
+    {
+        $this->createTable('users', [
+            'id' => 'pk',
+            'name' => Schema::TYPE_STRING . ' NOT NULL',
+            'email' => Schema::TYPE_STRING . ' NOT NULL',
+            'password' => Schema::TYPE_STRING . ' NOT NULL',
+            'auth_key' => Schema::TYPE_STRING . ' NOT NULL',
+            'access_token' => Schema::TYPE_STRING . ' NOT NULL',
+        ]);
+        $this->createIndex('email_i', 'users', 'email', true);
+        $this->createIndex('auth_key_i', 'users', 'auth_key');
+    }
+
+    public function down()
+    {
+        $this->dropTable('users');
+    }
+}
